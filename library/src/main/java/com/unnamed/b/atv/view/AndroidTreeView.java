0b37daada4594954a39fcf25a064a7b14b29122c
Niwin Santhosh <niwin@avednakrea.com>
2016-08-26 15:16:05 +0530

    Added includeSubnodes param to collapseNode method
    (cherry picked from commit 58b0d07e408626447a3a4b2bc89f89993a04d225 from https://github.com/vajro/AndroidTreeView)
    
    Added includeSubnodes param to collapseNode method
    
    (cherry picked from commit 68cc01267551e6e806c9ec4c20bea3c515a6baa6)

--- library/src/main/java/com/unnamed/b/atv/view/AndroidTreeView.java
+++ library/src/main/java/com/unnamed/b/atv/view/AndroidTreeView.java
@@ -182,8 +182,12 @@ public class AndroidTreeView {
         expandNode(node, false);
     }
 
-    public void collapseNode(TreeNode node, Boolean includeSubnodes) {
-        collapseNode(node, includeSubnodes);
+    public void collapseNodeWithSubnodes(TreeNode node) {
+        collapseNode(node, true);
+    }
+
+    public void collapseNode(TreeNode node) {
+        collapseNode(node, false);
     }
 
     public String getSaveState() {
